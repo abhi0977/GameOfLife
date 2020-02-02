39b07df1bf376ee3cefd67a71774aaca4c239746
Abhi <abhiramani3@gmail.com>
2020-02-02 13:27:11 +0530

    initialize grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let grid;
+let rows = 10;
+let columns = 10;
+
+const make2DArray = (rows, columns) => {
+    let arr = new Array(columns);
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = new Array(columns);
+    }
+    return arr;
+}
+
+const initializeRandomValue = () => {
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < columns; j++){
+            grid[i][j] = floor(random(2))
+        }
+    }
+}
+
 function setup() {
-    createCanvas(400, 400);
+    grid = make2DArray(rows, columns);
+    initializeRandomValue();
 }
 
 function draw() {
+    
     background(220);
 }
